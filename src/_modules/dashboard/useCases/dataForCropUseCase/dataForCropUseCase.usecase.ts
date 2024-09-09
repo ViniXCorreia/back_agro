afc1631beafe49d60018eae648aa8e46654b15a0
Vinícius Martins Correia <vinix.correia@gmail.com>
2024-09-08 22:32:19 -0300

    adjust import cropsDatadto

--- src/_modules/dashboard/useCases/dataForCropUseCase/dataForCropUseCase.usecase.ts
+++ src/_modules/dashboard/useCases/dataForCropUseCase/dataForCropUseCase.usecase.ts
@@ -4,7 +4,7 @@ import { RepositoryProxyModule } from 'src/infra/database/proxy/repository.proxy
 import { Repository } from 'typeorm';
 import { IDataForCropUseCase } from './dataForCropUseCase.inteface';
 import { RuralProducerEntity } from 'src/infra/database/entities/rural_producer.entity';
-import { CropsDataDto } from '../../infra/dto/CropsData.dto';
+import { CropsDataDto } from 'src/_modules/dashboard/infra/dto/CropsData.dto';
 import { CropDataReturnDto } from '../../infra/dto/cropsDataReturn.dto';
 
 export class DataForCropUseCase implements IDataForCropUseCase {
